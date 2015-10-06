6a67d575a97e1ca64691ceb7b17709f7fac56592
kibin ᕕ( ᐛ )ᕗ <mr.kibin@gmail.com>
2015-10-06 22:04:35 +0300

    grunt work done

---
+++ dev_server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let path = require('path');
+let express = require('express');
+let webpack = require('webpack');
+let config = require('./webpack.config');
+
+let app = express();
+let compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, 'localhost', (err) => {
+  if (err) return console.error(err);
+
+  console.info('Listening at http://localhost:3000');
+});
