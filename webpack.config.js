14fb6b4033e83773a0d3600224dc7585bec5a169
kibin ᕕ( ᐛ )ᕗ <mr.kibin@gmail.com>
2015-10-06 23:23:46 +0300

    fix configs, add styles handling

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
-var path = require('path');
-var webpack = require('webpack');
+'use strict';
+
+let path = require('path');
+let webpack = require('webpack');
+let autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-hot-middleware/client',
-    './src/index'
+    './src/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.ProvidePlugin({
       'React': 'react',
-      'R': 'ramda'
-    })
+      'R': 'ramda',
+    }),
   ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+      include: path.join(__dirname, 'src'),
+    }, {
+      test: /\.styl$/,
+      loader: 'style!css!postcss!stylus',
+      include: path.join(__dirname, 'src'),
+    }],
+  },
+  postcss: [
+    autoprefixer({ browsers: [ 'last 2 versions' ] }),
+  ],
 };
